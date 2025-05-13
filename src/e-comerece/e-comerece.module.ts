c648284c7e80dddd3db6f5c2f60a2b305a36b4f0
MFaad1 <faadsardar123@gmail.com>
2025-05-13 17:16:28 +0500

    ecomerece section added

--- src/e-comerece/e-comerece.module.ts
+++ src/e-comerece/e-comerece.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { EComereceService } from './e-comerece.service';
 import { EComereceResolver } from './e-comerece.resolver';
-import { PrismaModule } from 'prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   providers: [EComereceResolver, EComereceService],
