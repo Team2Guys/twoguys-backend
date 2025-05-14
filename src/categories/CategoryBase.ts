5de2eedb5328b275a25de0b83b4b7d078017d959
MFaad1 <faadsardar123@gmail.com>
2025-05-14 16:52:46 +0500

    added admins api and ecomerece procucts in the categories array

--- src/categories/CategoryBase.ts
+++ src/categories/CategoryBase.ts
@@ -2,7 +2,7 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { GraphQLJSON } from 'graphql-type-json';
 import { Subcategory } from '../subcategories/entities/subcategory.entity';
 import { Product } from '../products/entities/product.entity';
-import { EComerece } from 'e-comerece/entities/e-comerece.entity';
+import { EComerece } from '../e-comerece/entities/e-comerece.entity';
 
 @ObjectType({ isAbstract: true })
 export abstract class  CategoryBase  {
