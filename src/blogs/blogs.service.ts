c427c1620533a3c4602475961c78b272eb6d1e0e
Sharjeel Faiq <sharjeelfaiq816@gmail.com>
2026-01-22 17:07:24 +0500

    changed path alias of utils directory with relative paths

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -7,7 +7,7 @@ import {
 } from "./dto/create-blog.input";
 import { UpdateBlogInput, UpdateCommentDto } from "./dto/update-blog.input";
 import { PrismaService } from "../prisma/prisma.service";
-import { customHttpException, getStatusNameByCode } from "utils/helper";
+import { customHttpException, getStatusNameByCode } from "../utils/helper";
 import { CommentStatus } from "../general/dto/enums/enum";
 import { randomUUID } from "crypto";
 import { Prisma } from "../../generated/prisma";
